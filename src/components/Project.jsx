73bc66d7d7fb019e78aec77fbb509c571c219aee
mbman11 <m.behrmann11@gmail.com>
2023-11-01 19:18:31 -0600

    added styles to project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,32 @@
 const styles = {
   projectContent: {
     display: 'flex',
+    flexDirection: 'column',
+    width: '100vw',
     background: 'purple',
-    height: '50vh'
+    height: '50vh',
+
+  },
+  paragraph: {
+    display: 'flex',
+    background: 'blue',
+
   }
 }
 
 function Project() {
   return (
     <div className='projectContent' style={styles.projectContent}>
+
     <h1>About Me</h1>
+    <div className="paragraph" style={styles.paragraph}>
+    <p>
+    jkashdfjsadkjhkjsdhafkjsahdfkjshgasdknfnbasdkjhgsadjkhgadsjhfhjsadgjdsahgfjkhdsgfjkdsagfjksadhfjksdagfjksdagfjkshgfjkhdsfhgasdfjkgsdakjfgsjfhgsdahfgskajdfgksjadgksjdahkjsagfkjdsajkfgdsakjh</p>
+    </div>
+
+
     </div>
   )
 }
-
+ 
 export default Project;
