bd6c37b96b5d27b2dcee2613ced4e190ff8b6c65
mbman11 <m.behrmann11@gmail.com>
2023-11-01 18:36:00 -0600

    added footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,24 @@
 const styles = {
   footer: {
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '80px',
     listStyle: 'none',
     background: 'pink', 
     position: 'fixed',
     bottom: '0',
     left: '0',
-    width: '100%'
+    width: '100%',
+    gap: '50px'
   }
 }
 
 function Footer() {
   return (
     <footer className="footer" style={styles.footer}>
-      <a><li>idk</li></a>
-      <a><li>idk</li></a>
-      <a><li>idk</li></a>
+      <a><li>Github</li></a>
+      <a><li>Linkedin</li></a>
     </footer>
   );
 }
